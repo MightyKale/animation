67f00cd69565954f10dde8a9e71ccdb4ba107e09
MightyKale <achuxx622@gmail.com>
2023-09-07 11:12:38 -0700

    for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
-  // basePath: '/metaverse', 
-  // assetPrefix: '/metaverse/', 
+  basePath: '/metaverse', 
+  assetPrefix: '/metaverse/', 
 };
 
 module.exports = nextConfig;
